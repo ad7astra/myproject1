82b930bd18cf4793ea9d835e0f0fd0ca6aa9f8ee
ad7astra <91362349+ad7astra@users.noreply.github.com>
2021-10-17 12:40:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+app.get('/:name', function (req, res) {
+  res.send('hello' + req.params.name);
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
